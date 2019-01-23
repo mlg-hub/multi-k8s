e31a7a597102d519e2cb7a9331381483d92b7253
morisho <morisho.ongala@gmail.com>
2019-01-23 11:55:32 +0200

    fix bugs again

--- client/src/App.js
+++ client/src/App.js
@@ -7,31 +7,23 @@ import Fib from './Fib';
 
 class App extends Component {
     render() {
-        return ( <
-            Router >
-            <
-            div className = "App" >
-            <
-            header className = "App-header" >
-            <
-            img src = { logo }
+        return (
+        <Router>
+            <div className = "App" >
+            <header className = "App-header" >
+            <img src = { logo }
             className = "App-logo"
             alt = "logo" / >
-            <
-            h1 className = "App-title" > Comment etre bon! < /h1> <
-            Link to = "/" > Home < /Link> <Link to = "/otherpage
-            " > Other Page </Link>  < /
-            header > <
-            div >
-            <
-            Route exact path = "/"
-            component = { Fib }
-            /> <
+            <h1 className = "App-title" > Comment etre bon! </h1>
+            <Link to = "/" > Home </Link> <Link to = "/otherpage" >Other Page </Link>
+            </header>
+            <div >
+            <Route exact path = "/"component = { Fib }/> <
             Route path = "/otherpage"
             component = { OtherPage }
-            /> < /
-            div > < /div > < /
-            Router >
+            /></div >
+            </div >
+            </Router >
         );
     }
 }
